8e28fc6cf94687d6696f3ed46b2820d510a365f8
thy <972143891@qq.com>
2019-08-08 18:01:46 +0800

    feat: add simple cors

--- server/router.js
+++ server/router.js
@@ -16,6 +16,15 @@ module.exports = {
       }
     });
     // res.setHeader("Content-Type", "text/html;charset=utf-8");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type,Content-Length,Authorization,Accept,X-Requested-With"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "PUT,POST,GET,DELETE,OPTIONS"
+    );
     res.writeHead(200, "ok");
     res.end(data);
   }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-var test = {a:test}
+fetch("http://localhost:9001/getData")
+  .then(response => response.json())
+  .then(res => {
+    console.info('res',res)
+  })
+  .catch(error => {
+    console.log("Request Error", error);
+  });
